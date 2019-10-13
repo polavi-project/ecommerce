eb6c031baf7b0276cb0d8046cbbaefb5abb39006
similik <56462322+similik@users.noreply.github.com>
2019-10-13 22:56:54 +0700

    Fix shipping method checked

--- public/theme/front/default/js/dev/flat_rate/flat_rate.js
+++ public/theme/front/default/js/dev/flat_rate/flat_rate.js
@@ -4,7 +4,6 @@ export default function FlatRate(props) {
     const shippingAddress = ReactRedux.useSelector(state => _.get(state, 'appState.cart.shippingAddress'));
     const shippingMethod = ReactRedux.useSelector(state => _.get(state, 'appState.cart.shippingMethod'));
     const allowCountries = _.get(props, 'countries');
-    const [checked, setChecked] = React.useState(shippingMethod === 'flat_rate');
 
     const onChange = (e) => {
         e.preventDefault();
@@ -20,7 +19,7 @@ export default function FlatRate(props) {
         props.areaProps.setNoMethod(false);
         return <div className="shipping-method flat-rate">
             <label htmlFor={"free-shipping"}>
-                <input type={"radio"} className="uk-radio" checked={checked} onChange={(e)=>onChange(e)}/>
+                <input type={"radio"} className="uk-radio" checked={shippingMethod === 'flat_rate'} onChange={(e)=>onChange(e)}/>
                  {_.get(props, 'label', 'Flat rate')} - {_.get(props, 'fee')}
             </label>
         </div>;
--- public/theme/front/default/js/production/flat_rate/flat_rate.js
+++ public/theme/front/default/js/production/flat_rate/flat_rate.js
@@ -4,7 +4,6 @@ export default function FlatRate(props) {
     const shippingAddress = ReactRedux.useSelector(state => _.get(state, 'appState.cart.shippingAddress'));
     const shippingMethod = ReactRedux.useSelector(state => _.get(state, 'appState.cart.shippingMethod'));
     const allowCountries = _.get(props, 'countries');
-    const [checked, setChecked] = React.useState(shippingMethod === 'flat_rate');
 
     const onChange = e => {
         e.preventDefault();
@@ -19,7 +18,7 @@ export default function FlatRate(props) {
             React.createElement(
                 'label',
                 { htmlFor: "free-shipping" },
-                React.createElement('input', { type: "radio", className: 'uk-radio', checked: checked, onChange: e => onChange(e) }),
+                React.createElement('input', { type: "radio", className: 'uk-radio', checked: shippingMethod === 'flat_rate', onChange: e => onChange(e) }),
                 _.get(props, 'label', 'Flat rate'),
                 ' - ',
                 _.get(props, 'fee')
