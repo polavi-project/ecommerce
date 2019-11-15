6b7a6432a4cc0db356bd960d022b9eab2c9f526c
similik <56462322+similik@users.noreply.github.com>
2019-11-15 23:09:49 +0700

    Styling Flatrate setting

--- public/theme/admin/default/js/dev/flat_rate/setting/flat_rate_form.js
+++ public/theme/admin/default/js/dev/flat_rate/setting/flat_rate_form.js
@@ -26,48 +26,49 @@ export default function FlatRateSettingForm(props) {
         setShowForm(!showForm);
     };
 
-    return <div>
-        <p>
-            Flat Rate
-            <a onClick={(e) => onClick(e)}>Edit</a>
-        </p>
-        <div style={{display: showForm ? 'block' : 'none'}}>
-            <Form
-                id={"flat_rate-setting-form"}
-                {...props}>
-                <Area id="flat_rate-setting-form" coreWidgets={[
-                    {
-                        component: Text,
-                        props : {id : 'shipment_flat_rate_name', value: _.get(props, 'shipment_flat_rate_name', ''), formId: "flat_rate-setting-form", name: "shipment_flat_rate_name", label: "Title", validation_rules:["notEmpty"]},
-                        sort_order: 10,
-                        id: "shipment_flat_rate_name"
-                    },
-                    {
-                        component: Text,
-                        props : {id : 'shipment_flat_rate_fee', value: _.get(props, 'shipment_flat_rate_fee', ''), formId: "flat_rate-setting-form", name: "shipment_flat_rate_fee", label: "Shipping fee", validation_rules:["notEmpty"]},
-                        sort_order: 20,
-                        id: "shipment_flat_rate_fee"
-                    },
-                    {
-                        component: Select,
-                        props : {id : 'shipment_flat_rate_status', value: _.get(props, 'shipment_flat_rate_status', ''), formId: "flat_rate-setting-form", name: "shipment_flat_rate_status", label: "Status", options:[{value:0, text:'Disabled'}, {value:1, text:'Enabled'}], isTranslateAble:false},
-                        sort_order: 30,
-                        id: "shipment_flat_rate_status"
-                    },
-                    {
-                        component: Country,
-                        props : {id : 'shipment_flat_rate_countries', countries: _.get(props, 'shipment_flat_rate_countries', []), formId: "flat_rate-setting-form"},
-                        sort_order: 40,
-                        id: "shipment_flat_rate_countries"
-                    },
-                    {
-                        component: Text,
-                        props : {id : 'shipment_flat_rate_sort_order', value: _.get(props, 'shipment_flat_rate_sort_order', ''), formId: "flat_rate-setting-form", name: "shipment_flat_rate_sort_order", label: "Sort order"},
-                        sort_order: 50,
-                        id: "shipment_flat_rate_sort_order"
-                    }
-                ]}/>
-            </Form>
+    return <div className="uk-width-1-3">
+        <div className="border-block">
+            <div>
+                Flat Rate  <a onClick={(e) => onClick(e)}>Edit</a>
+            </div>
+            <div style={{display: showForm ? 'block' : 'none'}}>
+                <Form
+                    id={"flat_rate-setting-form"}
+                    {...props}>
+                    <Area id="flat_rate-setting-form" coreWidgets={[
+                        {
+                            component: Text,
+                            props : {id : 'shipment_flat_rate_name', value: _.get(props, 'shipment_flat_rate_name', ''), formId: "flat_rate-setting-form", name: "shipment_flat_rate_name", label: "Title", validation_rules:["notEmpty"]},
+                            sort_order: 10,
+                            id: "shipment_flat_rate_name"
+                        },
+                        {
+                            component: Text,
+                            props : {id : 'shipment_flat_rate_fee', value: _.get(props, 'shipment_flat_rate_fee', ''), formId: "flat_rate-setting-form", name: "shipment_flat_rate_fee", label: "Shipping fee", validation_rules:["notEmpty"]},
+                            sort_order: 20,
+                            id: "shipment_flat_rate_fee"
+                        },
+                        {
+                            component: Select,
+                            props : {id : 'shipment_flat_rate_status', value: _.get(props, 'shipment_flat_rate_status', ''), formId: "flat_rate-setting-form", name: "shipment_flat_rate_status", label: "Status", options:[{value:0, text:'Disabled'}, {value:1, text:'Enabled'}], isTranslateAble:false},
+                            sort_order: 30,
+                            id: "shipment_flat_rate_status"
+                        },
+                        {
+                            component: Country,
+                            props : {id : 'shipment_flat_rate_countries', countries: _.get(props, 'shipment_flat_rate_countries', []), formId: "flat_rate-setting-form"},
+                            sort_order: 40,
+                            id: "shipment_flat_rate_countries"
+                        },
+                        {
+                            component: Text,
+                            props : {id : 'shipment_flat_rate_sort_order', value: _.get(props, 'shipment_flat_rate_sort_order', ''), formId: "flat_rate-setting-form", name: "shipment_flat_rate_sort_order", label: "Sort order"},
+                            sort_order: 50,
+                            id: "shipment_flat_rate_sort_order"
+                        }
+                    ]}/>
+                </Form>
+            </div>
         </div>
     </div>
 }
--- public/theme/admin/default/js/production/flat_rate/setting/flat_rate_form.js
+++ public/theme/admin/default/js/production/flat_rate/setting/flat_rate_form.js
@@ -34,51 +34,55 @@ export default function FlatRateSettingForm(props) {
 
     return React.createElement(
         "div",
-        null,
-        React.createElement(
-            "p",
-            null,
-            "Flat Rate",
-            React.createElement(
-                "a",
-                { onClick: e => onClick(e) },
-                "Edit"
-            )
-        ),
+        { className: "uk-width-1-4" },
         React.createElement(
             "div",
-            { style: { display: showForm ? 'block' : 'none' } },
+            { className: "border-block" },
             React.createElement(
-                Form,
-                _extends({
-                    id: "flat_rate-setting-form"
-                }, props),
-                React.createElement(Area, { id: "flat_rate-setting-form", coreWidgets: [{
-                        component: Text,
-                        props: { id: 'shipment_flat_rate_name', value: _.get(props, 'shipment_flat_rate_name', ''), formId: "flat_rate-setting-form", name: "shipment_flat_rate_name", label: "Title", validation_rules: ["notEmpty"] },
-                        sort_order: 10,
-                        id: "shipment_flat_rate_name"
-                    }, {
-                        component: Text,
-                        props: { id: 'shipment_flat_rate_fee', value: _.get(props, 'shipment_flat_rate_fee', ''), formId: "flat_rate-setting-form", name: "shipment_flat_rate_fee", label: "Shipping fee", validation_rules: ["notEmpty"] },
-                        sort_order: 20,
-                        id: "shipment_flat_rate_fee"
-                    }, {
-                        component: Select,
-                        props: { id: 'shipment_flat_rate_status', value: _.get(props, 'shipment_flat_rate_status', ''), formId: "flat_rate-setting-form", name: "shipment_flat_rate_status", label: "Status", options: [{ value: 0, text: 'Disabled' }, { value: 1, text: 'Enabled' }], isTranslateAble: false },
-                        sort_order: 30,
-                        id: "shipment_flat_rate_status"
-                    }, {
-                        component: Country,
-                        props: { id: 'shipment_flat_rate_countries', countries: _.get(props, 'shipment_flat_rate_countries', []), formId: "flat_rate-setting-form" },
-                        sort_order: 40,
-                        id: "shipment_flat_rate_countries"
-                    }, {
-                        component: Text,
-                        props: { id: 'shipment_flat_rate_sort_order', value: _.get(props, 'shipment_flat_rate_sort_order', ''), formId: "flat_rate-setting-form", name: "shipment_flat_rate_sort_order", label: "Sort order" },
-                        sort_order: 50,
-                        id: "shipment_flat_rate_sort_order"
-                    }] })
+                "div",
+                null,
+                "Flat Rate  ",
+                React.createElement(
+                    "a",
+                    { onClick: e => onClick(e) },
+                    "Edit"
+                )
+            ),
+            React.createElement(
+                "div",
+                { style: { display: showForm ? 'block' : 'none' } },
+                React.createElement(
+                    Form,
+                    _extends({
+                        id: "flat_rate-setting-form"
+                    }, props),
+                    React.createElement(Area, { id: "flat_rate-setting-form", coreWidgets: [{
+                            component: Text,
+                            props: { id: 'shipment_flat_rate_name', value: _.get(props, 'shipment_flat_rate_name', ''), formId: "flat_rate-setting-form", name: "shipment_flat_rate_name", label: "Title", validation_rules: ["notEmpty"] },
+                            sort_order: 10,
+                            id: "shipment_flat_rate_name"
+                        }, {
+                            component: Text,
+                            props: { id: 'shipment_flat_rate_fee', value: _.get(props, 'shipment_flat_rate_fee', ''), formId: "flat_rate-setting-form", name: "shipment_flat_rate_fee", label: "Shipping fee", validation_rules: ["notEmpty"] },
+                            sort_order: 20,
+                            id: "shipment_flat_rate_fee"
+                        }, {
+                            component: Select,
+                            props: { id: 'shipment_flat_rate_status', value: _.get(props, 'shipment_flat_rate_status', ''), formId: "flat_rate-setting-form", name: "shipment_flat_rate_status", label: "Status", options: [{ value: 0, text: 'Disabled' }, { value: 1, text: 'Enabled' }], isTranslateAble: false },
+                            sort_order: 30,
+                            id: "shipment_flat_rate_status"
+                        }, {
+                            component: Country,
+                            props: { id: 'shipment_flat_rate_countries', countries: _.get(props, 'shipment_flat_rate_countries', []), formId: "flat_rate-setting-form" },
+                            sort_order: 40,
+                            id: "shipment_flat_rate_countries"
+                        }, {
+                            component: Text,
+                            props: { id: 'shipment_flat_rate_sort_order', value: _.get(props, 'shipment_flat_rate_sort_order', ''), formId: "flat_rate-setting-form", name: "shipment_flat_rate_sort_order", label: "Sort order" },
+                            sort_order: 50,
+                            id: "shipment_flat_rate_sort_order"
+                        }] })
+                )
             )
         )
     );
