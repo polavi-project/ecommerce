bb849c49fb63b6d49a61dd8b866c49ce24d42b2c
similik <56462322+similik@users.noreply.github.com>
2019-11-30 23:17:45 +0700

    Fix create new page button

--- public/theme/admin/default/js/dev/cms/page/grid/add_new_button.js
+++ public/theme/admin/default/js/dev/cms/page/grid/add_new_button.js
@@ -1,5 +1,5 @@
 import A from "../../../../../../../../js/production/a.js";
 
 export default function AddNewButton({url}) {
-    return <A pushState={true} text={"New coupon"} url={url} className={"uk-button uk-button-primary uk-button-small"}/>
+    return <A pushState={true} text={"New page"} url={url} className={"uk-button uk-button-primary uk-button-small"}/>
 }
