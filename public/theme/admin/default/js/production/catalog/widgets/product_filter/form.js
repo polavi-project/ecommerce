7141a2a58f2809dddaa48236bd8841037ad6e249
similik <56462322+similik@users.noreply.github.com>
2020-04-16 23:08:44 +0700

    Product filter widget edit #144

--- public/theme/admin/default/js/production/catalog/widgets/product_filter/form.js
+++ public/theme/admin/default/js/production/catalog/widgets/product_filter/form.js
@@ -1,11 +1,11 @@
 import Text from "../../../../../../../../js/production/form/fields/text.js";
-import Select from "../../../../../../../../js/production/form/fields/select.js";
 import { AreaList } from "../../../../production/cms/widget/area_list.js";
 import { Form } from "../../../../../../../../js/production/form/form.js";
 import { ADD_ALERT } from "../../../../../../../../js/production/event-types.js";
 import { Fetch } from "../../../../../../../../js/production/fetch.js";
 import { LayoutList } from "../../../../production/cms/widget/layout_list.js";
 import Radio from "../../../../../../../../js/production/form/fields/radio.js";
+import Switch from "../../../../../../../../js/production/form/fields/switch.js";
 
 export default function ProductFilter({ id, name, status, setting = [], displaySetting, sortOrder, formAction, areaProps, redirect }) {
     const layout = _.find(displaySetting, { key: 'layout' }) !== undefined ? JSON.parse(_.get(_.find(displaySetting, { key: 'layout' }), 'value', [])) : [];
@@ -25,89 +25,114 @@ export default function ProductFilter({ id, name, status, setting = [], displayS
 
     return React.createElement(
         "div",
-        { className: "uk-margin-medium-top" },
+        { className: "mt-4" },
         React.createElement(
             Form,
             {
                 id: "product-filter-widget-edit-form",
                 action: formAction,
-                onComplete: onComplete
+                onComplete: onComplete,
+                submitText: null
             },
             React.createElement(
-                "h3",
-                null,
-                "Product filter widget"
-            ),
-            React.createElement(
                 "div",
-                { className: "uk-child-width-1-2 uk-grid" },
+                { className: "row" },
                 React.createElement(
                     "div",
-                    null,
-                    React.createElement("input", { type: "text", name: "query", value: "mutation CreateTextWidget($widget: WidgetInput!) { createWidget (widget: $widget) {status message}}", readOnly: true, style: { display: 'none' } }),
-                    React.createElement("input", { type: "text", name: "variables[widget][type]", value: "product_filter", readOnly: true, style: { display: 'none' } }),
-                    id && React.createElement("input", { type: "text", name: "variables[widget][id]", value: id, readOnly: true, style: { display: 'none' } }),
-                    React.createElement(Text, {
-                        name: "variables[widget][name]",
-                        value: name,
-                        formId: "text-widget-edit-form",
-                        validation_rules: ['notEmpty'],
-                        label: "Name"
-                    }),
-                    React.createElement(Select, {
-                        name: "variables[widget][status]",
-                        value: status,
-                        formId: "text-widget-edit-form",
-                        options: [{ value: '1', text: 'Enable' }, { value: '0', text: 'Disable' }],
-                        label: "Status"
-                    }),
-                    React.createElement("input", { type: "text", name: "variables[widget][setting][0][key]", value: "title", readOnly: true, style: { display: 'none' } }),
-                    React.createElement(Text, {
-                        name: "variables[widget][setting][0][value]",
-                        value: _.get(_.find(setting, { key: 'title' }), 'value', ""),
-                        formId: "product-filter-widget-edit-form",
-                        validation_rules: ['notEmpty'],
-                        label: "Title"
-                    }),
-                    React.createElement("input", { type: "text", name: "variables[widget][setting][1][key]", value: "show_count", readOnly: true, style: { display: 'none' } }),
+                    { className: "col-8" },
                     React.createElement(
                         "div",
-                        null,
+                        { className: "sml-block" },
                         React.createElement(
-                            "span",
+                            "div",
+                            { className: "sml-block-title" },
+                            "Product filter widget"
+                        ),
+                        React.createElement(
+                            "div",
                             null,
-                            "Show product count?"
+                            React.createElement("input", { type: "text", name: "query", value: "mutation CreateTextWidget($widget: WidgetInput!) { createWidget (widget: $widget) {status message}}", readOnly: true, style: { display: 'none' } }),
+                            React.createElement("input", { type: "text", name: "variables[widget][type]", value: "product_filter", readOnly: true, style: { display: 'none' } }),
+                            id && React.createElement("input", { type: "text", name: "variables[widget][id]", value: id, readOnly: true, style: { display: 'none' } }),
+                            React.createElement(Text, {
+                                name: "variables[widget][name]",
+                                value: name,
+                                formId: "text-widget-edit-form",
+                                validation_rules: ['notEmpty'],
+                                label: "Name"
+                            }),
+                            React.createElement(Switch, {
+                                name: "variables[widget][status]",
+                                value: status,
+                                formId: "text-widget-edit-form",
+                                label: "Status"
+                            }),
+                            React.createElement("input", { type: "text", name: "variables[widget][setting][0][key]", value: "title", readOnly: true, style: { display: 'none' } }),
+                            React.createElement(Text, {
+                                name: "variables[widget][setting][0][value]",
+                                value: _.get(_.find(setting, { key: 'title' }), 'value', ""),
+                                formId: "product-filter-widget-edit-form",
+                                validation_rules: ['notEmpty'],
+                                label: "Title"
+                            }),
+                            React.createElement("input", { type: "text", name: "variables[widget][setting][1][key]", value: "show_count", readOnly: true, style: { display: 'none' } }),
+                            React.createElement(
+                                "div",
+                                null,
+                                React.createElement(
+                                    "span",
+                                    null,
+                                    "Show product count?"
+                                )
+                            ),
+                            React.createElement(Radio, {
+                                name: "variables[widget][setting][1][value]",
+                                value: _.get(_.find(setting, { key: 'show_count' }), 'value', ''),
+                                formId: "product-filter-widget-edit-form",
+                                validation_rules: ['notEmpty'],
+                                options: [{ value: '1', text: 'Yes' }, { value: '0', text: 'No' }]
+                            })
+                        ),
+                        React.createElement(
+                            "div",
+                            { className: "mt-4 text-right" },
+                            React.createElement(
+                                "button",
+                                { type: "submit", className: "btn btn-primary" },
+                                "Submit"
+                            )
                         )
-                    ),
-                    React.createElement(Radio, {
-                        name: "variables[widget][setting][1][value]",
-                        value: _.get(_.find(setting, { key: 'show_count' }), 'value', ''),
-                        formId: "product-filter-widget-edit-form",
-                        validation_rules: ['notEmpty'],
-                        options: [{ value: '1', text: 'Yes' }, { value: '0', text: 'No' }]
-                    })
+                    )
                 ),
                 React.createElement(
                     "div",
-                    null,
+                    { className: "col-4" },
                     React.createElement(
-                        "h3",
-                        null,
-                        "Select page layout"
+                        "div",
+                        { className: "sml-block" },
+                        React.createElement(
+                            "div",
+                            { className: "sml-block-title" },
+                            "Select page layout"
+                        ),
+                        React.createElement(LayoutList, { formId: "text-widget-edit-form", selectedLayouts: layout })
                     ),
-                    React.createElement(LayoutList, { formId: "text-widget-edit-form", selectedLayouts: layout }),
                     React.createElement(
-                        "h3",
-                        null,
-                        "Select area"
-                    ),
-                    React.createElement(AreaList, { formId: "text-widget-edit-form", selectedAreas: area }),
-                    React.createElement(Text, {
-                        name: "variables[widget][sort_order]",
-                        value: sortOrder,
-                        formId: "text-widget-edit-form",
-                        label: "Sort order"
-                    })
+                        "div",
+                        { className: "sml-block mt-4" },
+                        React.createElement(
+                            "div",
+                            { className: "sml-block-title" },
+                            "Select area"
+                        ),
+                        React.createElement(AreaList, { formId: "text-widget-edit-form", selectedAreas: area }),
+                        React.createElement(Text, {
+                            name: "variables[widget][sort_order]",
+                            value: sortOrder,
+                            formId: "text-widget-edit-form",
+                            label: "Sort order"
+                        })
+                    )
                 )
             )
         )
