d598bce8a8ee29c9a156ce7a6d46e97ccc1ed0df
similik <56462322+similik@users.noreply.github.com>
2020-04-16 17:33:47 +0700

    Text widget #144

--- public/theme/admin/default/js/production/cms/widget/edit/widget_edit.js
+++ public/theme/admin/default/js/production/cms/widget/edit/widget_edit.js
@@ -5,31 +5,35 @@ function TypeSelector({ types, setType, type = "", id = null }) {
 
     return React.createElement(
         "div",
-        null,
+        { className: "form-head sticky" },
         React.createElement(
-            "h3",
-            null,
-            "Please select widget type"
-        ),
-        React.createElement(
-            "select",
-            {
-                onChange: e => setType(e.target.value),
-                className: "uk-input uk-select uk-input-medium",
-                value: type
-            },
+            "div",
+            { className: "form-group" },
             React.createElement(
-                "option",
-                { value: "", disabled: true },
+                "label",
+                null,
                 "Please select widget type"
             ),
-            types.map((t, i) => {
-                return React.createElement(
+            React.createElement(
+                "select",
+                {
+                    onChange: e => setType(e.target.value),
+                    className: "form-control",
+                    value: type
+                },
+                React.createElement(
                     "option",
-                    { value: t.code, key: i },
-                    t.name
-                );
-            })
+                    { value: "", disabled: true },
+                    "Please select widget type"
+                ),
+                types.map((t, i) => {
+                    return React.createElement(
+                        "option",
+                        { value: t.code, key: i },
+                        t.name
+                    );
+                })
+            )
         )
     );
 }
@@ -37,39 +41,26 @@ function TypeSelector({ types, setType, type = "", id = null }) {
 export default function WidgetEditForm({ types, selectedType = "", widgetId = null }) {
     const [type, setType] = React.useState(selectedType);
 
-    return React.createElement(
-        "div",
-        null,
-        React.createElement(Area, {
-            id: "widget_edit_page",
-            coreWidgets: [{
-                component: () => React.createElement(
-                    "h2",
-                    null,
-                    !widgetId ? "Create new widget" : "Edit widget"
-                ),
-                props: {},
-                sort_order: 10,
-                id: "page_title"
-            }, {
-                component: TypeSelector,
-                props: {
-                    types,
-                    type,
-                    id: widgetId,
-                    setType: setType
-                },
-                sort_order: 10,
-                id: "type_selector"
-            }, {
-                component: Area,
-                props: {
-                    id: "widget_edit_form",
-                    type: type
-                },
-                sort_order: 20,
-                id: "widget_form"
-            }]
-        })
-    );
+    return React.createElement(Area, {
+        id: "widget_edit_page",
+        coreWidgets: [{
+            component: TypeSelector,
+            props: {
+                types,
+                type,
+                id: widgetId,
+                setType: setType
+            },
+            sort_order: 10,
+            id: "type_selector"
+        }, {
+            component: Area,
+            props: {
+                id: "widget_edit_form",
+                type: type
+            },
+            sort_order: 20,
+            id: "widget_form"
+        }]
+    });
 }
