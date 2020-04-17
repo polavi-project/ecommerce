a5fb2b717a071dc85085caa01b3d62f4965d6413
similik <56462322+similik@users.noreply.github.com>
2020-04-17 17:50:09 +0700

    COD payment form #144

--- public/theme/admin/default/js/production/setting/payment/payment.js
+++ public/theme/admin/default/js/production/setting/payment/payment.js
@@ -1,21 +1,8 @@
 import Area from "../../../../../../../js/production/area.js";
 
 export default function Payment() {
-    return React.createElement(
-        "div",
-        null,
-        React.createElement(
-            "div",
-            null,
-            React.createElement(
-                "h2",
-                null,
-                "Payment methods"
-            )
-        ),
-        React.createElement(Area, {
-            id: "payment-setting",
-            className: "uk-grid-small uk-grid"
-        })
-    );
+    return React.createElement(Area, {
+        id: "payment-setting",
+        className: "row"
+    });
 }
