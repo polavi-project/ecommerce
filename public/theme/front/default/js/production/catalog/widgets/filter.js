12caa89b72e1e48acf62f6a369557f82ee96c2be
similik <56462322+similik@users.noreply.github.com>
2020-04-30 23:46:54 +0700

    Price filter improvement

--- public/theme/front/default/js/production/catalog/widgets/filter.js
+++ public/theme/front/default/js/production/catalog/widgets/filter.js
@@ -2,67 +2,103 @@ import Area from "../../../../../../../js/production/area.js";
 import { Fetch } from "../../../../../../../js/production/fetch.js";
 import { buildFilterToQuery } from "../product/list/buildFilterToQuery.js";
 
-function Price({ minPrice, maxPrice, maxSteps = 3, minRange = 50, areaProps }) {
-    const getSteps = () => {
-        let stepNumber = Math.min((parseFloat(maxPrice) - parseFloat(minPrice)) / parseFloat(minRange), maxSteps);
-        if (stepNumber <= 1) return [{ from: minPrice, to: maxPrice }];else {
-            let step = Math.round((parseFloat(maxPrice) - parseFloat(minPrice)) / stepNumber);
-            let steps = [];
-            let prev = minPrice;
-            for (let i = 0; i < stepNumber; i++) {
-                let from = prev;
-                let to = Math.round(prev + step);
-                prev = to;
-                if (to > maxPrice) to = maxPrice;
-                steps.push({ from: from, to: to });
-            }
+function Price({ minPrice = "", maxPrice = "", areaProps }) {
+    const [min, setMin] = React.useState(null);
+    const [max, setMax] = React.useState(null);
+    const currentFilter = ReactRedux.useSelector(state => {
+        return _.get(state, 'appState.productCollectionFilter.price.value', `${minPrice},${maxPrice}`).split(",");
+    });
+    const currency = ReactRedux.useSelector(state => _.get(state, 'appState.currency', 'USD'));
 
-            return steps;
-        }
+    React.useEffect(() => {
+        setMin(currentFilter[0]);
+        setMax(currentFilter[1]);
+    }, [minPrice, maxPrice]);
+
+    const onChangeMin = e => {
+        setMin(e.target.value);
     };
 
-    const currency = ReactRedux.useSelector(state => _.get(state, 'appState.currency', 'USD'));
-    const language = ReactRedux.useSelector(state => _.get(state, 'appState.language[0]', 'en'));
-    const steps = getSteps();
+    const onChangeMax = e => {
+        setMax(e.target.value);
+    };
+
     return React.createElement(
         "div",
-        { className: "row" },
+        { className: "price-filter" },
         React.createElement(
             "div",
-            { className: "header price-header" },
+            { className: "title uk-margin-small-bottom" },
+            React.createElement(
+                "strong",
+                null,
+                "Price"
+            )
+        ),
+        React.createElement(
+            "div",
+            { className: "filter uk-margin-small-bottom" },
             React.createElement(
                 "div",
-                { className: "title uk-margin-small-bottom" },
+                { className: "uk-flex" },
                 React.createElement(
-                    "strong",
+                    "div",
                     null,
-                    "Price"
-                )
-            ),
-            React.createElement(
-                "div",
-                { className: "filter uk-margin-small-bottom" },
-                steps.map((s, i) => {
-                    const _from = new Intl.NumberFormat(language, { style: 'currency', currency: currency }).format(s.from);
-                    const _to = new Intl.NumberFormat(language, { style: 'currency', currency: currency }).format(s.to);
-                    return React.createElement(
+                    React.createElement(
                         "div",
-                        { key: i },
+                        null,
                         React.createElement(
-                            "a",
-                            { href: "#", onClick: e => {
-                                    e.preventDefault();areaProps.updateFilter('price', 'BETWEEN', `${s.from} AND ${s.to}`);
-                                } },
-                            React.createElement(
-                                "span",
-                                null,
-                                _from,
-                                " to ",
-                                _to
-                            )
+                            "span",
+                            null,
+                            "From"
                         )
-                    );
-                })
+                    ),
+                    React.createElement(
+                        "div",
+                        { className: "uk-inline" },
+                        React.createElement(
+                            "span",
+                            { className: "uk-form-icon" },
+                            currency
+                        ),
+                        React.createElement("input", {
+                            type: "text",
+                            className: "uk-input uk-form-small",
+                            value: min,
+                            onChange: e => onChangeMin(e),
+                            onBlur: () => areaProps.updateFilter("price", "BETWEEN", `${min}, ${max}`)
+                        })
+                    )
+                ),
+                React.createElement(
+                    "div",
+                    null,
+                    React.createElement(
+                        "div",
+                        null,
+                        React.createElement(
+                            "span",
+                            null,
+                            "To"
+                        )
+                    ),
+                    React.createElement(
+                        "div",
+                        { className: "uk-inline" },
+                        React.createElement(
+                            "span",
+                            { className: "uk-form-icon" },
+                            currency
+                        ),
+                        React.createElement("input", {
+                            type: "text",
+                            className: "uk-input uk-form-small",
+                            value: max,
+                            onChange: e => onChangeMax(e),
+                            onBlur: () => areaProps.updateFilter("price", "BETWEEN", `${min}, ${max}`)
+                        })
+                    )
+                )
             )
         )
     );
@@ -193,7 +229,7 @@ export default function Filter({ title }) {
         filters: productCollectionFilter,
         coreWidgets: [{
             component: Price,
-            props: { minPrice: _.get(data, 'price.minPrice', null), maxPrice: _.get(data, 'price.maxPrice', null) },
+            props: { minPrice: _.get(data, 'price.minPrice', ""), maxPrice: _.get(data, 'price.maxPrice', "") },
             sort_order: 10,
             id: "filter-price"
         }, {
