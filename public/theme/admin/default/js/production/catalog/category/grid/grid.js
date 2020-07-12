65aab0fb17b399809d2333d6e7160db08ea21b6e
The Nguyen <56462322+similik@users.noreply.github.com>
2020-07-12 22:22:26 +0700

    194

--- public/theme/admin/default/js/production/catalog/category/grid/grid.js
+++ public/theme/admin/default/js/production/catalog/category/grid/grid.js
@@ -458,6 +458,6 @@ export default function CategoryGrid({ apiUrl, areaProps }) {
             null,
             "There is no category to display"
         ),
-        React.createElement(Pagination, { total: total, currentFilters: areaProps.filters, setFilter: areaProps.updateFilter })
+        React.createElement(Pagination, { total: total, page: _.get(areaProps.filters.find(e => e.key === 'page'), 'value', 1), limit: _.get(areaProps.filters.find(e => e.key === 'limit'), 'value', 20), setFilter: areaProps.updateFilter })
     );
 }
