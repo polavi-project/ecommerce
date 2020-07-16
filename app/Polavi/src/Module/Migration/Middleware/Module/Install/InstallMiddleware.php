4c1ebd528630d18aa7acbb8e7baa2cefc6765b70
The Nguyen <56462322+similik@users.noreply.github.com>
2020-07-16 13:41:54 +0700

    #54 part 2

--- app/Polavi/src/Module/Migration/Middleware/Module/Install/InstallMiddleware.php
+++ app/Polavi/src/Module/Migration/Middleware/Module/Install/InstallMiddleware.php
@@ -9,25 +9,29 @@ declare(strict_types=1);
 namespace Polavi\Module\Migration\Middleware\Module\Install;
 
 
+use function PHPSTORM_META\elementType;
 use function Polavi\_mysql;
 use Polavi\Middleware\MiddlewareAbstract;
 use Polavi\Services\Http\Request;
 use Polavi\Services\Http\Response;
 
-class ValidateMiddleware extends MiddlewareAbstract
+class InstallMiddleware extends MiddlewareAbstract
 {
 
     public function __invoke(Request $request, Response $response, $delegate = null)
     {
         try {
             $module = $request->attributes->get("module");
+            $path = file_exists(MODULE_PATH . DS . $module) ? MODULE_PATH . DS . $module : COMMUNITY_MODULE_PATH . DS . $module;
             if(!preg_match('/^[A-Za-z0-9_]+$/', $module))
                 throw new \Exception("Invalid module name");
-            if(!file_exists(COMMUNITY_MODULE_PATH . DS . $module) && !file_exists(MODULE_PATH . DS . $module))
-                throw new \Exception("Module folder could not be found");
+            include $path . DS . $module . DS . "migration.php";
+
             $conn = _mysql();
-            if($conn->getTable("migration")->where("module", "LIKE", $module)->fetchOneAssoc())
-                throw new \Exception(sprintf("Module %s is already installed", $module));
+            $conn->getTable("migration")->insert([
+                "module" => $module,
+                "version" => $
+            ]);
 
             return $delegate;
         } catch (\Exception $e) {
