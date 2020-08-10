124bfa6a8058322a59fbe12c3ef66d22fa30d79c
The Nguyen <56462322+similik@users.noreply.github.com>
2020-08-10 22:42:00 +0700

    :smiley: Improve product single image block

--- public/theme/front/default/js/dev/catalog/product/view/images.js
+++ public/theme/front/default/js/dev/catalog/product/view/images.js
@@ -5,14 +5,15 @@ function MainImage({src, alt}) {
 }
 
 export default function Images({images, productName}) {
-    let mainImage = images.find(function(i) {
+    const [mainImage, setMainImage] = React.useState(images.find(function(i) {
         return i.isMain === true;
-    });
+    }));
+
     return <div className="product-single-media">
         {mainImage && <MainImage src={mainImage.main} alt={productName}/>}
         <ul className="more-view-thumbnail product-gallery">
             {images.map((i,j) => {
-                return <li key={j}><a href={"#"}><img src={i.thumb} alt={productName}/></a></li>
+                return <li key={j}><a href={"#"} onClick={(e) => {e.preventDefault(); setMainImage({...i});}}><img src={i.thumb} alt={productName}/></a></li>
             })}
         </ul>
     </div>
