8248fa3af1e7100e4d4d1f9d590b82866d3a2e95
The Nguyen <56462322+similik@users.noreply.github.com>
2020-08-15 22:49:23 +0700

    import A from ../../../../../../../js/production/a.js;

--- public/theme/admin/default/js/dev/order/dashboard/best_sellers.js
+++ public/theme/admin/default/js/dev/order/dashboard/best_sellers.js
@@ -1,6 +1,7 @@
 import A from "../../../../../../../js/production/a.js";
 
 export default function BestSellers({products, listUrl}) {
+    const currency = ReactRedux.useSelector(state => _.get(state, 'appState.currency'));
     return <div className="sml-block mt-4">
         <div className="sml-block-title sml-flex-space-between">
             <div>Best sellers</div>
@@ -8,19 +9,20 @@ export default function BestSellers({products, listUrl}) {
         </div>
         <table className="table table-bordered">
             <thead>
-                <tr>
-                    <th>Product name</th>
-                    <th>Sku</th>
-                    <th>Price</th>
-                    <th>Sold Quantity</th>
-                </tr>
+            <tr>
+                <th>Product name</th>
+                <th>Sku</th>
+                <th>Price</th>
+                <th>Sold Quantity</th>
+            </tr>
             </thead>
             <tbody>
             {products.map((p, i) => {
+                const _price = new Intl.NumberFormat('en', { style: 'currency', currency: currency }).format(p.price);
                 return <tr key={i}>
                     <td><A url={p.editUrl}>{p.name}</A></td>
                     <td>{p.sku}</td>
-                    <td>{p.price}</td>
+                    <td>{_price}</td>
                     <td>{p.qty}</td>
                 </tr>
             })}
