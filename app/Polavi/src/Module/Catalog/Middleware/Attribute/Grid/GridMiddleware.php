8c362de8f55ef67b1bf66ee1b5cd485ab8741d29
The Nguyen <56462322+similik@users.noreply.github.com>
2020-08-21 14:30:21 +0700

    :simple_smile: Fix coding standard

--- app/Polavi/src/Module/Catalog/Middleware/Attribute/Grid/GridMiddleware.php
+++ app/Polavi/src/Module/Catalog/Middleware/Attribute/Grid/GridMiddleware.php
@@ -24,22 +24,25 @@ class GridMiddleware extends MiddlewareAbstract
      */
     public function __invoke(Request $request, Response $response, $delegate = null)
     {
-        if ($response->hasWidget('attribute-grid'))
+        if ($response->hasWidget('attribute-grid')) {
             return $delegate;
+        }
 
         $this->getContainer()->get(Helmet::class)->setTitle("Product attributes");
 
         $response->addWidget(
             'attribute_grid_container',
             'content',
-            0, get_js_file_url("production/grid/grid.js", true),
+            0,
+            get_js_file_url("production/grid/grid.js", true),
             ['id'=>"attribute_grid_container"]
         );
 
         $response->addWidget(
             'attribute_grid',
             'attribute_grid_container',
-            10, get_js_file_url("production/catalog/attribute/grid/grid.js", true),
+            10,
+            get_js_file_url("production/catalog/attribute/grid/grid.js", true),
             [
                 "apiUrl" => generate_url('admin.graphql.api')
             ]
